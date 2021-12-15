63d5d28281d647b86fb90db1fc0ae918584dde1c
Tananatham <73059344+Tananatham@users.noreply.github.com>
2021-12-15 23:01:32 +0800

    update

---
+++ src/scenes/Game.js
@@ -0,0 +1,16 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+    preload() {
+
+    }
+
+    create() {
+        const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+        this.physics.add.existing(ball)
+
+        ball.body.setCollideWorldBounds(True, 1, 1)
+
+        ball.body.setVelocity(200, 200)
+    }
+}
