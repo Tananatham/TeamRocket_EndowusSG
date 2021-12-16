624da52bf4ad5c6d9521c3ac5f93e6f0a425dd00
Tananatham <73059344+Tananatham@users.noreply.github.com>
2021-12-16 10:25:35 +0800

    update

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,15 +2,27 @@ import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
     preload() {
+        this.preload.image('tiles','./src/assets/overworld.png')
 
     }
 
     create() {
-        const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-        this.physics.add.existing(ball)
+        const array = [
+                        [0, 1, 2],
+                        [0, 1, 2],
+                        [0, 1, 2]
+        ]
+        const map = this.make.tilemap({ data:array, tileWidth: 64, tileHeight: 64}) 
+        map.addTilesetImage('tiles', 64, 64)
 
-        ball.body.setCollideWorldBounds(True, 1, 1)
+        const layer = map.createLayer(0, 'tiles', 0, 0)
 
-        ball.body.setVelocity(200, 200)
+
+        // const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+        // this.physics.add.existing(ball)
+
+        // ball.body.setCollideWorldBounds(True, 1, 1)
+
+        // ball.body.setVelocity(200, 200)
     }
 }
